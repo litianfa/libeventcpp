f0b96052953fb3d6dce68a90428bd043119f0c14
litianfa <tianfali096@gmail.com>
2019-11-04 09:57:57 +0800

    Initial Commit

---
+++ libeventcpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  libeventcpp
+//
+//  Created by litianfa on 2019/11/4.
+//  Copyright © 2019 litianfa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
